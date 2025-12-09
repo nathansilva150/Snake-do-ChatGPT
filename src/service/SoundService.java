4938a6838427006897a5c1eb1f499021b6c84641
Nathan Silva <nathan_silva150@estudante.sesisenai.org.br>
2025-12-09 20:19:53 -0300

    Adição de novos sons

--- src/service/SoundService.java
+++ src/service/SoundService.java
@@ -13,10 +13,20 @@ public class SoundService {
 			"resources/sounds/eats/eatMunch.wav",
 			"resources/sounds/eats/eatMinecraft.wav",
 			"resources/sounds/eats/eatRoblox.wav",
+			"resources/sounds/eats/eatYEHAH.wav",
+			"resources/sounds/eats/eatMiBombo.wav",
+			"resources/sounds/eats/eatMickey.wav",
+			"resources/sounds/eats/eatVineBoom.wav",
+			"resources/sounds/eats/eatRapaz.wav",
 			"resources/sounds/eats/eatHeavyTf2.wav");
 	
 	private final List<String> gameOverSounds = List.of(
 			"resources/sounds/trilhaSonora/telefonezinho.wav",
+			"resources/sounds/trilhaSonora/welcomeToTheMato.wav",
+			"resources/sounds/trilhaSonora/apanharCalado.wav",
+			"resources/sounds/trilhaSonora/peidaoLongo.wav",
+			"resources/sounds/trilhaSonora/naoChoraxx.wav",
+			"resources/sounds/trilhaSonora/flamengoFiuFiu.wav",
 			"resources/sounds/trilhaSonora/xenogenesis.wav",
 			"resources/sounds/trilhaSonora/zoio-duvido.wav",
 			"resources/sounds/trilhaSonora/chegouMensagem.wav"
